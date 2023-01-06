004d100334c60cee47f56e6cc5cda944a5cf3fcc
ekrembel <ekrem.bel1@gmail.com>
2023-01-05 19:41:29 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Col, Container, Row, Table } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserTable />
   );
+}
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <Container fluid='md'>
+        <Row className="mx-0">
+          <Button as={Col} variant="primary">Button #1</Button>
+          <Button as={Col} xs={6} variant="secondary" className="mx-2">Button #2</Button>
+          <Button as={Col} variant="success">Button #3</Button>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+class UserTable extends React.Component {
+  render () {
+    return (
+      <Container fluid='md'>
+        <hr></hr>
+        <TodoList />
+        <br></br>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan={2}>Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Container>
+    )
+  }
 }
 
 export default App;
